8696f3ca60b992c815df19c56667a73a4717101e
Morshaedul <morshaedulanirbanit@gmail.com>
2022-08-29 20:59:00 +0600

    first commit

---
+++ Js/index.js
@@ -0,0 +1,30 @@
+function myFunction(x, y) {
+    if (y === undefined) {
+      y = 2;
+    }  
+    return x * y;
+  }
+  document.getElementById("demo").innerHTML = myFunction(4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var date =new Date();
+// var currentDate=date.getFullYear();
+// var Date=date.getDay();
+// document.write(currentDate);
+// document.write(date);
